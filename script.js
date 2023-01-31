a0cddfc5fd998dacfdc69a8eba6ef3ccd0eb102f
vinicius bispo <vini.bispo125@gmail.com>
2023-01-30 23:39:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+//---1º---//
+
+function exibir(){
+    console.log("Óla Tudo Bem ?")
+}
+ exibir()
+
+//---2º---//
+
+function nome(nome){
+    console.log(nome)
+    
+}
+ nome("Vinicius")
+
+//---3º---//
+
+function info(nome,idade,musica){
+    console.log(nome)
+    console.log(idade)
+    console.log(musica)
+   }
+ info("Vinicius","23 anos","Rap")
+
+//---4º---//
+
+function info2(filme,musica){
+    console.log(filme)
+    console.log(musica)
+}   
+ info2("Interestelar","Emicida - Levanta e Anda")
+
+//---5º---//
+
+function triplo(numero){
+    numero *=3
+    console.log(numero)
+}
+ triplo(6)
+
+//---6º---//
+
+ let i = function (numero1,numero2){
+    return numero1 >= numero2   
+ }
+ console.log(i(1,2))
+ 
